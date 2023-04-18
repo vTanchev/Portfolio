00c2a9130d6b9d93cf0b0d89324279853f97f6ac
vTanchev <vaskotanchev@yahoo.com>
2023-04-18 02:47:28 +0200

    form component

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,7 +1,18 @@
+import { Container, Typography } from "@mui/material";
 import React from "react";
 
 const Contact = () => {
-  return <div id="contact">Contact</div>;
+  return (
+    <React.Fragment>
+      <Container id="contact">
+        <Typography variant="h5">Contact</Typography>
+        <Typography variant="body1">
+          Feel free to Contact me by submitting the form below and I will get
+          back to you as soon as possible
+        </Typography>
+      </Container>
+    </React.Fragment>
+  );
 };
 
 export default Contact;
